9c7d8b1409e7cbd283c55ba216d72c937441203a
municorn <municorn@musicaloft.com>
2023-03-30 21:43:22 -0500

    Display a muni on screen :)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use bevy::{prelude::*, winit::WinitSettings};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(WinitSettings::desktop_app())
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let img_handle: Handle<Image> = asset_server.load("png_muni_quiet.png");
+
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(ImageBundle {
+        image: UiImage::new(img_handle),
+        style: Style {
+            position_type: PositionType::Absolute,
+            position: UiRect {
+                bottom: Val::Px(32.),
+                right: Val::Px(32.),
+                ..Default::default()
+            },
+            size: Size::width(Val::Px(256.)),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
 }
