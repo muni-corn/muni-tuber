368190ecdb46adad83fac4f2e468eab707299174
municorn <municorn@musicaloft.com>
2023-07-28 11:08:21 -0500

    flake: Formatting

--- flake.nix
+++ flake.nix
@@ -8,66 +8,72 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, fenix, naersk }:
-    let
-      appName = "muni-tuber";
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    fenix,
+    naersk,
+  }: let
+    appName = "muni-tuber";
 
-      out = utils.lib.eachDefaultSystem
-        (system:
-          let
-            pkgs = import nixpkgs { inherit system; };
+    out =
+      utils.lib.eachDefaultSystem
+      (system: let
+        pkgs = import nixpkgs {inherit system;};
 
-            rust = fenix.packages.${system}.complete;
-            naersk-lib = naersk.lib."${system}".override {
-              inherit (rust) cargo rustc;
-            };
+        rust = fenix.packages.${system}.complete;
+        naersk-lib = naersk.lib."${system}".override {
+          inherit (rust) cargo rustc;
+        };
 
-            nativeBuildInputs = with pkgs; [
-              rust.toolchain
-              pkg-config
-            ];
-            buildInputs = with pkgs; [
-              alsa-lib
-              udev
-              vulkan-loader
+        nativeBuildInputs = with pkgs; [
+          rust.toolchain
+          pkg-config
+        ];
+        buildInputs = with pkgs; [
+          alsa-lib
+          udev
+          vulkan-loader
 
-              # x11
-              xorg.libX11
-              xorg.libXcursor
-              xorg.libXi
-              xorg.libXrandr
+          # x11
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
 
-              # wayland
-              libxkbcommon
-              wayland
-            ];
-          in
-          {
-            # `nix build`
-            defaultPackage = naersk-lib.buildPackage {
-              pname = appName;
-              root = builtins.path {
-                path = ./.;
-                name = "${appName}-src";
-              };
-              inherit nativeBuildInputs buildInputs;
-            };
+          # wayland
+          libxkbcommon
+          wayland
+        ];
+      in {
+        # `nix build`
+        defaultPackage = naersk-lib.buildPackage {
+          pname = appName;
+          root = builtins.path {
+            path = ./.;
+            name = "${appName}-src";
+          };
+          inherit nativeBuildInputs buildInputs;
+        };
 
-            # `nix run`
-            defaultApp = utils.lib.mkApp {
-              name = appName;
-              drv = self.defaultPackage."${system}";
-              exePath = "/bin/${appName}";
-            };
+        # `nix run`
+        defaultApp = utils.lib.mkApp {
+          name = appName;
+          drv = self.defaultPackage."${system}";
+          exePath = "/bin/${appName}";
+        };
 
-            # `nix develop`
-            devShell = with pkgs; mkShell {
-              inherit nativeBuildInputs buildInputs;
-              LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
-            };
-          });
-    in
-    out // {
+        # `nix develop`
+        devShell = with pkgs;
+          mkShell {
+            inherit nativeBuildInputs buildInputs;
+            LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+          };
+      });
+  in
+    out
+    // {
       overlay = final: prev: {
         ${appName} = self.defaultPackage.${prev.system};
       };
